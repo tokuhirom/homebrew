f4325fe92e264637109536f191896ba6a3081516
tokuhirom <tokuhirom@gmail.com>
2013-05-30 17:47:54 +0900

    plenv 1.9.2

--- Library/Formula/plenv.rb
+++ Library/Formula/plenv.rb
@@ -2,17 +2,21 @@ require 'formula'
 
 class Plenv < Formula
   homepage 'https://github.com/tokuhirom/plenv'
-  url 'https://github.com/tokuhirom/plenv/archive/1.4.15.tar.gz'
-  sha1 '34c8524b990010c6d1d1d8508b880904b327fe03'
+  url 'https://github.com/tokuhirom/plenv/archive/1.9.2.zip'
+  sha1 'ba4e17695157a1f88257b3393e9bc6178fc5ac29'
 
   head 'https://github.com/tokuhirom/plenv.git'
 
   def install
-    prefix.install 'bin', 'share'
+    inreplace 'libexec/plenv', '/usr/local', HOMEBREW_PREFIX
+    prefix.install Dir['*']
   end
 
   def caveats; <<-EOS.undent
-    To enable shims add to your profile:
+    To use Homebrew's directories rather than ~/.plenv add to your profile:
+      export PLENV_ROOT=#{var}/plenv
+
+    To enable shims and autocompletion add to your profile:
       if which plenv > /dev/null; then eval "$(plenv init -)"; fi
     EOS
   end
